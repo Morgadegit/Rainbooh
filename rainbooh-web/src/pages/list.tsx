ed8d47ad0caaa97d0c684c34fae02659a5c158e1
Morgadegit <raphard.sacha@gmail.com>
2021-12-07 11:02:34 -0500

    added commented out line for test

--- rainbooh-web/src/pages/list.tsx
+++ rainbooh-web/src/pages/list.tsx
@@ -5,9 +5,15 @@ import { useAdDisplayQuery } from "../generated/graphql";
 export const List = () => {
 
     const ad = useAdDisplayQuery()[0].data?.adDisplay;
-    console.log(ad)
+    console.log(ad);
+    // console.log(ad[0].companyName);
     return(<>
-    <NavBar />
+        <NavBar />
+        <ul>
+        {ad?.forEach(element => {
+            <li>{element.companyName}</li>            
+        })};
+        </ul>
     </>)
 }
 
